45cbbd2a59e1f477aa8876664f3fb41a96f94505
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2019-06-28 16:49:25 +0200

    Extend test case to test multiple markets at once

--- tests/test_decentralized_da.R
+++ tests/test_decentralized_da.R
@@ -11,8 +11,9 @@ test_that("Test decentralized DA",{
     }
   }
   
-  nStudents <- 200
-  nColleges <- 40
+  nruns = 1
+  nStudents <- 2000
+  nColleges <- 150
   mean <- nStudents/nColleges
   sd <- mean/2
   
@@ -20,8 +21,8 @@ test_that("Test decentralized DA",{
     sapply(round(rnorm(n, mean=mean, sd=sd)), function(x) max(1,x))
   }
   nSlots <- capacityfun(nColleges, mean, sd)
-  sum(nSlots)
-  daresult <- stabsim3(m=1, nStudents=nStudents, nSlots=nSlots, verbose=FALSE, 
+  
+  daresult <- stabsim3(m=nruns, nStudents=nStudents, nSlots=nSlots, verbose=FALSE, 
              colleges = c("cx","cy","opening"), 
              students = c("sx", "sy", "idist", "iopen", "age"),
              colleges_fun = c(equaldist,equaldist,category(2)), 
@@ -29,18 +30,23 @@ test_that("Test decentralized DA",{
              outcome = ~ I(sqrt((cx-sx)**2 + (cy-sy)**2)), 
              selection = c(
                student = ~ I(idist * sqrt((cx-sx)**2 + (cy-sy)**2)) + I(iopen * opening),
-               colleges = ~ I(age/20) + I(sqrt((cx-sx)**2 + (cy-sy)**2))),private_college_quota = 0.1)
-  damatching <- daresult$OUT
+               colleges = ~ I(age/20) + I(sqrt((cx-sx)**2 + (cy-sy)**2))),private_college_quota = 0.4)
   
-  expect_gt(daresult$iterations[[1]],1)
+  show(unlist(daresult$iterations))
   
-  hriresult <- hri(s.prefs = daresult$s.prefs,c.prefs = daresult$c.prefs, nSlots=nSlots)
-  hrimatching <- hriresult$matchings[hriresult$matchings$cOptimal == 1,]
-  
-  merged <- merge(damatching, hrimatching, by.x=c('s.id'), by.y=c('student'))
-  
-  equal_allocations <- sum(merged$c.id == merged$college) 
-  num_allocations <- length(merged$c.id)
-  
-  expect_equal(equal_allocations, num_allocations)
+  for (i in 1:nruns) {
+    damatching = daresult$OUT[[i]]
+
+    expect_gt(daresult$iterations[[1]],1)
+
+    hriresult <- hri(s.prefs = daresult$s.prefs[[i]], c.prefs = daresult$c.prefs[[i]], nSlots=nSlots)
+    hrimatching <- hriresult$matchings[hriresult$matchings$cOptimal == 1,]
+
+    merged <- merge(damatching, hrimatching, by.x=c('s.id'), by.y=c('student'))
+
+    equal_allocations <- sum(merged$c.id == merged$college) 
+    num_allocations <- length(merged$c.id)
+
+    expect_equal(equal_allocations, num_allocations)
+  }
 })
