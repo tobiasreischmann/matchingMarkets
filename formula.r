d1868eb8e4464d8e85981df337e626321f152028
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2019-07-09 18:25:44 +0200

    Current state of formula for evaluation

--- formula.r
+++ formula.r
@@ -6,10 +6,10 @@ category <- function(c) {
     round(runif(x) * c + 0.5)
   }
 }
-
+for (i in c(50,100,150,200,300,400)) {
 nruns = 1
 nStudents <- 2000
-nColleges <- 180
+nColleges <- i
 mean <- nStudents/nColleges
 sd <- mean/2
 
@@ -19,34 +19,45 @@ capacityfun <- function(n, mean, sd=1) {
 nSlots <- capacityfun(nColleges, mean, sd)
 
 results <- matrix(nrow = 10, ncol = 2)
-areasize <- 40
+
 colnames(results) <- c('value', 'result')
-for (i in seq(1,11)) {
-quota <- (i - 1)/10
+quota <- 0.4#(i - 1)/10
+
+areasize <- ceiling(sqrt(i/3))
+private <- function(x) {
+  runif(x) < quota
+}
 daresult <- stabsim3(m=nruns, nStudents=nStudents, nSlots=nSlots, verbose=TRUE, 
-                     colleges = c("cx","cy","opening", "provider"), 
-                     students = c("sx", "sy", "idist", "iopen", "iprovider1", "iprovider2", "iprovider3", "iprovider4", "iprovider5", "age"),
-                     colleges_fun = c(category(areasize),category(areasize),category(2),category(5)), 
-                     students_fun = c(category(areasize),category(areasize),equaldist,equaldist,equaldist,equaldist,equaldist,equaldist,equaldist,category(20)),
-                     outcome = ~ I(sqrt(((cx-sx)/areasize)**2 + ((cy-sy)/areasize)**2)) + I(iopen * (opening-1)), 
+                     colleges = c("cx","cy", "firstpref", "secondpref", "quality", "cidiocat", "cidio1", "cidio2", "private"), 
+                     students = c("sx", "sy", "social", "sidiocat", "idist", "iidio", "sidio1", "sidio2", "iquality"),
+                     colleges_fun = c(category(areasize),category(areasize),category(3),category(2),equaldist,category(5),equaldist,equaldist,private), 
+                     students_fun = c(category(areasize),category(areasize),category(100),category(5),equaldist,equaldist,equaldist,equaldist,equaldist),
+                     outcome = ~ I(sqrt(((cx-sx)/areasize)**2 + ((cy-sy)/areasize)**2)), 
                      selection = c(
-                       student = ~ I(idist * sqrt(((cx-sx)/areasize)**2 + ((cy-sy)/areasize)**2)) + I(iopen * (opening-1))
-                       + I(iprovider1 * (provider==1))
-                       + I(iprovider2 * (provider==2))
-                       + I(iprovider3 * (provider==3))
-                       + I(iprovider4 * (provider==4))
-                       + I(iprovider5 * (provider==5)),
-                       colleges = ~ I(age/20) + I(sqrt(((cx-sx)/areasize)**2 + ((cy-sy)/areasize)**2))),private_college_quota = quota,
-                     count.waitinglist = function(x) {x}, s.prefs.count = 60)
+                       student = ~ I((1000**(firstpref %% 3)) * (abs(cx-sx)<=1) * (abs(cy-sy)<=1)) 
+                                 + I((1000**((firstpref + secondpref) %% 3)) * social)
+                                 + I((1000**((firstpref - secondpref) %% 3)) * private * ceiling((cidio1 + sidio1 %% 1) * 100)),
+                       colleges = ~ I(idist * sqrt(((cx-sx)/areasize)**2 + ((cy-sy)/areasize)**2))
+                                  + I(iquality * quality) 
+                                  + I(iidio * (cidiocat == sidiocat))
+                                  + I(iidio *(cidio2 + sidio2 %% 1))
+                       ),
+                     private_college_quota = quota,
+                     count.waitinglist = function(x) {x}, s.prefs.count = 40)
+
+  sapply(1:nColleges, function(x){
+       sum(sapply(1:nStudents, function(y){x %in% daresult$s.prefs[[1]][,y]})
+      )})
+  sapply(1:nColleges, function(x) {nSlots[x] - sum(daresult$OUT[[1]]$c.id == x)})
 
   sum(nSlots) - length(daresult$OUT[[1]]$s.id)
+  nStudents - length(daresult$OUT[[1]]$s.id)
   iteration <- daresult$iterations[[1]]
   iterationtable <- t(as.matrix(iteration[,-1]))
-  bp <- barplot(iterationtable, main="Development of held offers over number of iterations",
+  bp <- barplot(iterationtable, main=paste("Development of held offers over number of iterations (i=",i,")"),
               xlab="Number of iterations", col=c("darkblue","darkgreen","brown"),
               legend.text = row.names(iterationtable),ylim=c(0,1)) 
   axis(side = 1, at = bp, tick = FALSE, labels=seq(iterationtable[1,]))
-  results[i,1] <- quota
-  results[i,2] <- sum(iteration$new+iteration$altered > 0.05) + 1
+  #results[i,1] <- quota
+  #results[i,2] <- sum(iteration$new+iteration$altered > 0.05) + 1
 }
-
